601f5d78d8bf03b485ab7b1b6a498d97a40a1c58
EythordoNascimento <nascimentoeythor@gmail.com>
2025-04-30 16:09:30 -0300

    Enviando VALIDATIONS

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken')
 // helpers
 const createUserToken = require('../helpers/create-user-token')
 const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 
 module.exports = class UserController {
@@ -41,8 +42,6 @@ module.exports = class UserController {
         res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!'})
         return
       }
-
-
 
       // check if user exists
       const userExists = await User.findOne({ email: email})
@@ -139,9 +138,68 @@ module.exports = class UserController {
     res.status(200).json({ user})
   }
   static async editUser (req, res) {
-    res.status(200).json({
-      message: 'Deu certo update!',
-    })
-    return
+    const id = req.params.id
+    //check if user exists
+   
+    const {name, email, phone, password, confirmpassword } = req.body
+
+    let image = ''
+
+    // validations
+    if (!name){
+      res.status(422).json({ message: 'O nome é obrigatório'})
+      return
+    }
+
+    if (!email){
+      res.status(422).json({ message: 'O email é obrigatório'})
+      return
+    }
+
+    // check if email has already taken
+    const userExists = await User.findOne({email: email})
+    
+    if (user.email !== email && userExists) {
+      res.status(422).json({
+        message: 'Usuário não encontrado!'
+      })
+    }
+
+    user.email = email
+
+
+    if (!phone){
+      res.status(422).json({ message: 'O telefone é obrigatório'})
+      return
+    }
+
+    if (!password){
+      res.status(422).json({ message: 'A senha é obrigatória'})
+      return
+    }
+
+    if (!confirmpassword){
+      res.status(422).json({ message: 'A confirmação de senha é obrigatória'})
+      return
+    }
+
+    if(password !== confirmpassword){
+      res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!'})
+      return
+    }
+
+     
+
+    // check if user exists
+    const user = await User.findById(id)
+    
+    if (!user) {
+      res.status(422).json({
+        message: 'Usuário não encontrado!'
+      })
+    }
+
+
+
   }
 }
