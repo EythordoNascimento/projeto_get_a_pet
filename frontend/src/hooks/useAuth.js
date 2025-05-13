79e4ef55e439214d5375616780dc91bc6cb344bf
EythordoNascimento <nascimentoeythor@gmail.com>
2025-05-13 16:18:39 -0300

    Flash message

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,20 +1,30 @@
-import api from '../utils/api'
-
-import {useState, useEffect} from 'react'
-import {useHistory} from 'react-router-dom'
+import api from '../utils/api';
+import { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import useFlashMessage from './userFlashMessage';
 
 export default function useAuth() {
+    const { setFlashMessage } = useFlashMessage(); // Movendo para dentro do Hook personalizado
+
     async function register(user) {
+        let msgText = 'Cadastro realizado com sucesso!';
+        let msgType = 'Sucesso';
+
         try {
-            const data = await api.post('/users/register', user).then((response) => {
-             return response.data
-            })
-            console.log(data)
+            const data = await api.post('/users/register', user).then(response => response.data);
+            console.log(data);
         } catch (error) {
-            // tratar erro
-            console.log(error)
+            msgText = 'Ocorreu um erro inesperado';
+            msgType = 'Error';
+
+            // Verifica se error.response existe antes de acessar error.response.data
+            if (error.response && error.response.data) {
+                msgText = error.response.data.message;
+            }
+
+            setFlashMessage(msgText, msgType);
         }
     }
 
-    return { register }
+    return { register };
 }
