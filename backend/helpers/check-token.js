1c3a9b07a580f6129c7a58df735b31c41a951892
EythordoNascimento <nascimentoeythor@gmail.com>
2025-06-14 14:40:00 -0300

    Resolvendo conflitos de merge

--- backend/helpers/check-token.js
+++ backend/helpers/check-token.js
@@ -1,20 +1,16 @@
 const jwt = require("jsonwebtoken");
-const getUserByToken = require("../helpers/get-user-by-token"); // se você quiser reutilizar
-const getToken = require("../helpers/get-token");
 
-const checkToken = async (req, res, next) => {
-  const token = getToken(req);
+// middleware to validate token
+const checkToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) return res.status(401).json({ message: "Acesso negado!" });
 
   try {
-    const user = await getUserByToken(token);
-    if (!user) {
-      return res.status(401).json({ message: "Usuário não encontrado!" });
-    }
-
-    req.user = user;
-    next();
+    const verified = jwt.verify(token, "nossosecret");
+    req.user = verified;
+    next(); // to continue the flow
   } catch (err) {
     res.status(400).json({ message: "O Token é inválido!" });
   }
